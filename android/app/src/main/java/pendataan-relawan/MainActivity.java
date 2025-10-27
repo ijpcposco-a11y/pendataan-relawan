2f11f76e5bb61c60cb0af7f550ecec83dd4bda1b
ijpcposco-a11y <ijpcposco@gmail.com>
2025-10-27 12:39:30 +0700

    Delete android directory

--- android/app/src/main/java/pendataan-relawan/MainActivity.java
+++
@@ -1,7 +0,0 @@
-
-package com.example.checklistapp; // sesuaikan nama paket kamu
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- android/app/src/main/kotlin/namaproject/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-
-package com.example.pendataan_relawan; // ganti sesuai nama paket di AndroidManifest.xml
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
