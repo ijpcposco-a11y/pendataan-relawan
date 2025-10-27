cb74e22aed461769453c41c310289a3e83182df5
ijpcposco-a11y <ijpcposco@gmail.com>
2025-10-27 12:19:32 +0700

    Create MainActivity.kt

---
+++ android/app/src/main/kotlin/namaproject/MainActivity.kt
@@ -0,0 +1,7 @@
+
+package com.example.pendataan_relawan; // ganti sesuai nama paket di AndroidManifest.xml
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
