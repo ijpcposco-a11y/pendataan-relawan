3b1c98406ace1623e27109f91a88b7f381255a4e
ijpcposco-a11y <ijpcposco@gmail.com>
2025-10-28 15:18:37 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
 
     dependencies {
-        classpath "com.android.tools.build:gradle:8.7.0"
+        classpath "com.android.tools.build:gradle:8.1.0"
         classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24"
         classpath "com.google.gms:google-services:4.4.2"
     }
@@ -27,4 +27,5 @@ subprojects {
 tasks.register("clean", Delete) {
     delete rootProject.buildDir
 }
+
 
