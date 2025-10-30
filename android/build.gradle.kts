83686345a094fb7d81c4110583219c78f56ec88a
ijpcposco-a11y <ijpcposco@gmail.com>
2025-10-30 14:19:26 +0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,8 +6,13 @@ buildscript {
     }
 
     dependencies {
+        // Android Gradle Plugin terbaru stabil
         classpath "com.android.tools.build:gradle:8.6.0"
+
+        // Kotlin versi stabil untuk Flutter + plugin Firebase
         classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22"
+
+        // Google Services terbaru
         classpath "com.google.gms:google-services:4.4.3"
     }
 }
